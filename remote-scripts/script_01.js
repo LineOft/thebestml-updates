0b621f0980f25b11e213789365240cd2108476d2
TheBestML Updates <thebestml@updates.com>
2025-12-28 18:23:06 +0300

    chore: remove debug artifacts

--- remote-scripts/script_01.js
+++
@@ -1,8 +0,0 @@
-
-        window.onerror = function(msg, url, line, col, error) {
-            console.error('Global Hata:', msg, 'SatÄ±r:', line);
-            var errDiv = document.getElementById('globalError');
-            if (errDiv) errDiv.innerHTML = 'JS HatasÄ±: ' + msg + ' (SatÄ±r: ' + line + ')';
-            return false;
-        };
-    
